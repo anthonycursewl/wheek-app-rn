6c0b62dee67abf8d7c43a771b6d6823b3c9fd778
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-05 21:27:28 -0400

    feat: Update Wheek API Base URL to localhost

--- config/config.wheek.breadriuss.ts
+++ config/config.wheek.breadriuss.ts
@@ -1,3 +1,17 @@
+/*
+    Wheek's Config
+
+    Don't change this file unless you know what you're doing. 
+    If you need to change the API_BASE_URL, change it in the config.wheek.breadriuss.ts file.
+    @author Anthony Cursewl
+    @version 1.0.0
+    @license MIT
+    @copyright 2025 Breadriuss
+
+    https://wheek.breadriuss.com
+
+*/
+
 export const WheekConfig = {
-    API_BASE_URL: 'https://wheek.breadriuss.com/api',
+    API_BASE_URL: 'http://localhost/api',
 }
