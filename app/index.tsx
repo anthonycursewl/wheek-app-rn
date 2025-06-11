812a80a874c6b931e8d96e224961dab9b93a10a4
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-11 12:12:18 -0400

    feat: Refactor Login - Use RenderStepsLogin component for step rendering

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,14 @@
-import Button from "@/components/Buttons/Button";
-import Input from "@/components/Input/Input";
 import { router } from "expo-router";
 import { useEffect, useRef, useState } from 'react';
-import { Animated, Dimensions, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
-import CustomText from "../components/CustomText/CustomText";
+import { Animated, Image, View } from 'react-native';
 import useAuthStore from "@/flux/stores/AuthStore";
 import { loginAttemptAction, loginFailureAction, loginSuccessAction } from "@/flux/Actions/LoginActions";
 import { AuthService } from "@/flux/services/Auth/AuthService";
+import { renderStep } from "@/components/StepsAuth/RenderStepsLogin";
+import { stylesSteps } from "@/components/StepsAuth/styles";
 
-type LoginStep = 'email' | 'password' | 'success';
-
-
-const { width } = Dimensions.get('window');
 export default function Index() {
-    const [step, setStep] = useState<LoginStep>('email');
+    const [step, setStep] = useState<'email' | 'password' | 'success'>('email');
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -24,7 +19,7 @@ export default function Index() {
     const scaleAnim = useRef(new Animated.Value(0.95)).current;
 
     // global state
-    const { error, loading, dispatch, user } = useAuthStore();
+    const { error, loading, dispatch } = useAuthStore();
 
     useEffect(() => {
         fadeAnim.setValue(0);
@@ -77,211 +72,19 @@ export default function Index() {
         }
     };
 
-    const renderStep = () => {
-        switch (step) {
-            case 'email':
-                return (
-                    <Animated.View style={[styles.stepContainer, {
-                        opacity: fadeAnim,
-                        transform: [
-                            { translateY: slideAnim },
-                            { scale: scaleAnim }
-                        ]
-                    }]}>
-                        <View style={{ width: '100%', gap: 8 }}>
-                            <CustomText style={styles.label}>
-                                Correo Electrónico
-                            </CustomText>
-                            <Input 
-                                placeholder="ejemplo@email.com" 
-                                value={formData.email}
-                                onChangeText={(email) => setFormData({ ...formData, email })}
-                                autoCapitalize="none"
-                                keyboardType="email-address"
-                            />
-                            {error ? <CustomText style={styles.errorText}>{error}</CustomText> : null}
-                        </View>
-                        <View style={{ width: '100%', marginTop: 30 }}>
-                            <Button 
-                                title="Siguiente" 
-                                onPress={handleEmailSubmit}
-                            />
-                        </View>
-                    </Animated.View>
-                );
-            case 'password':
-                return (
-                    <Animated.View style={[styles.stepContainer, {
-                        opacity: fadeAnim,
-                        transform: [
-                            { translateY: slideAnim },
-                            { scale: scaleAnim }
-                        ]
-                    }]}>
-                        <View style={{ width: '100%', gap: 8 }}>
-                            <CustomText style={styles.label}>
-                                Contraseña
-                            </CustomText>
-                            <Input 
-                                placeholder="Ingresa tu contraseña..." 
-                                secureTextEntry 
-                                value={formData.password}
-                                onChangeText={(password) => setFormData({ ...formData, password })}
-                                onSubmitEditing={handlePasswordSubmit}
-                            />
-                            {error ? <CustomText style={styles.errorText}>{error}</CustomText> : null}
-                            <CustomText 
-                                style={styles.linkText}
-                                onPress={() => setStep('email')}
-                            >
-                                Cambiar correo
-                            </CustomText>
-                        </View>
-                        <View style={{ width: '100%', marginTop: 30 }}>
-                            <Button 
-                                disabled={loading || formData.password.length < 1}
-                                title="Iniciar sesión" 
-                                onPress={handlePasswordSubmit}
-                            />
-                        </View>
-                    </Animated.View>
-                );
-            case 'success':
-                return (
-                    <Animated.View style={[styles.successContainer, {
-                        opacity: fadeAnim,
-                        transform: [
-                            { translateY: slideAnim },
-                            { scale: scaleAnim }
-                        ]
-                    }]}>
-                        <View style={styles.decorationRight} />
-                            <Image
-                            source={require('../assets/images/wheek/wheek.png')} 
-                            style={styles.logo}
-                            />
-                        <View style={styles.decorationLeft} />
-
-                        <View>
-                            <CustomText style={styles.successText}>
-                                ¡Hola {user?.name.split(' ')[0]}!
-                            </CustomText>
-                            <CustomText style={styles.successSubtext}>Elige una sucursal.</CustomText>
-                        </View>
-
-                        <TouchableOpacity 
-                        style={{ width: '100%', marginTop: 15,
-                            borderWidth: 1,
-                            borderColor: 'rgb(148, 148, 148)',
-                            borderStyle: 'dashed',
-                            borderRadius: 12,
-                            padding: 10,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            height: 100,
-                         }}>
-                            <CustomText style={{ color: 'rgb(148, 148, 148)' }}>Tap para crear una sucursal.</CustomText>
-                        </TouchableOpacity>
-                        
-                    </Animated.View>
-                );
-        }
-    };
-
     return (
-        <View style={styles.container}>
+        <View style={stylesSteps.container}>
             {step !== 'success' && (
-                <View style={styles.logoContainer}>
+                <View style={stylesSteps.logoContainer}>
                     <Image 
                         source={require('@/assets/images/wheek/wheek.png')} 
-                        style={styles.logo}
+                        style={stylesSteps.logo}
                     />
                 </View>
             )}
-            {renderStep()}
+
+            {renderStep(step, fadeAnim, slideAnim, scaleAnim, formData, setFormData, error, handleEmailSubmit, handlePasswordSubmit, setStep, loading)}
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        backgroundColor: '#fff',
-    },
-    stepContainer: {
-        width: '85%',
-        alignItems: 'center',
-        transform: [{ scale: 1 }],
-    },
-    successContainer: {
-        flex: 1,
-        width: '100%',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        padding: 20,
-        transform: [{ scale: 1 }],
-    },
-    logoContainer: {
-        alignItems: 'center',
-        marginBottom: 40,
-        transform: [{ scale: 1 }],
-    },
-    logo: {
-        width: 150,
-        height: 50,
-        resizeMode: 'contain',
-        opacity: 1,
-        marginTop: 50,
-        transform: [{ scale: 1 }],
-    },
-    label: {
-        fontSize: 18,
-        color: '#000',
-        marginBottom: 8,
-    },
-    errorText: {
-        color: '#ff3b30',
-        marginTop: 5,
-        fontSize: 14,
-    },
-    linkText: {
-        color: '#007AFF',
-        marginTop: 10,
-        textAlign: 'right',
-    },
-    successText: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: '#000',
-        textAlign: 'center',
-    },
-    successSubtext: {
-        fontSize: 18,
-        color: '#666',
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    decorationRight: {
-        position: 'absolute',
-        right: -30,
-        top: 15,
-        width: width * 0.4,
-        height: width * 0.4,
-        backgroundColor: 'rgb(207, 148, 255)',
-        borderRadius: width * 0.2,
-        filter: 'blur(80px)',
-    },
-    decorationLeft: {
-        position: 'absolute',
-        left: -30,
-        top: 30,
-        width: width * 0.4,
-        height: width * 0.4,
-        backgroundColor: 'rgb(255, 143, 99)',
-        borderRadius: width * 0.2,
-        filter: 'blur(80px)',
-    },
-});
