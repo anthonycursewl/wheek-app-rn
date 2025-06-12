994217809b17a2d87d5bb5fd2566fb0d83b5fc10
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-12 16:47:17 -0400

    feat: Implement user logout in ProfileScreen

---
+++ components/dashboard/Screens/ProfileScreen.tsx
@@ -0,0 +1,21 @@
+import useAuthStore from "@/flux/stores/AuthStore"
+import LayoutScreen from "@/components/Layout/LayoutScreen"
+import Button from "@/components/Buttons/Button"
+import { logoutAttemptAction } from "@/flux/Actions/LoginActions"
+import { router } from "expo-router"
+import CustomText from "@/components/CustomText/CustomText"
+
+export const ProfileScreen = () => {
+    const { dispatch } = useAuthStore()
+  
+    return (
+      <LayoutScreen>
+        <Button title='Cerrar sesión' onPress={() => {
+            dispatch(logoutAttemptAction())
+            router.replace('/')
+        }} variant='binary-square' />
+  
+      <CustomText>Perfil</CustomText>
+    </LayoutScreen>
+    )
+  }
