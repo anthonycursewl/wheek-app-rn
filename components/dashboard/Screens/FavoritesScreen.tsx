eb686cfd1e4247b2ae4d73ecea51ba14d2db3eb7
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-12 16:46:05 -0400

    feat: Add FavoritesScreen with CustomText in LayoutScreen

---
+++ components/dashboard/Screens/FavoritesScreen.tsx
@@ -0,0 +1,8 @@
+import CustomText from "@/components/CustomText/CustomText";
+import LayoutScreen from "@/components/Layout/LayoutScreen";
+
+export const FavoritesScreen = () => (
+    <LayoutScreen>
+      <CustomText>Favoritos</CustomText>
+    </LayoutScreen>
+  );
