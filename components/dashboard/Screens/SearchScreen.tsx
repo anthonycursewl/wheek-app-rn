b2b63972777b886c7fde865392c46baef422a071
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-12 16:47:26 -0400

    feat: Add SearchScreen component

---
+++ components/dashboard/Screens/SearchScreen.tsx
@@ -0,0 +1,8 @@
+import CustomText from "@/components/CustomText/CustomText";
+import LayoutScreen from "@/components/Layout/LayoutScreen";
+
+export const SearchScreen = () => (
+    <LayoutScreen>
+      <CustomText>Búsqueda</CustomText>
+    </LayoutScreen>
+  );
