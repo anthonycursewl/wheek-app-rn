2019c46911b2f5565e7f31335a40f1de4b8c5914
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-13 13:33:50 -0400

    feat: Validates mandatory store fields on create and update

--- flux/services/StoreS/StoreService.ts
+++ flux/services/StoreS/StoreService.ts
@@ -4,6 +4,10 @@ import { secureFetch } from "@/hooks/http/useFetch";
 
 export const StoreService = {
     async createStore(dataObj: Omit<StoreData, 'id' | 'created_at' | 'is_active'>): Promise<{ data: StoreData | null, error: string | null }> {
+        if (!dataObj.name || !dataObj.description || !dataObj.owner) {
+            return { data: null, error: 'Todos los campos son obligatorios! Intenta de nuevo.' }
+        }
+
         const { data, error} = await secureFetch({
             options: {
                 url: `${WheekConfig.API_BASE_URL}/stores/create`,
@@ -16,10 +20,14 @@ export const StoreService = {
             return { data: null, error: error }
         }
 
-        return { data, error: null }
+        return { data: data.value, error: null }
     },
 
     async updateStore(dataObj: Omit<StoreData, 'id' | 'created_at' | 'is_active'>): Promise<{ data: StoreData | null, error: string | null }> {
+        if (!dataObj.name || !dataObj.description || !dataObj.owner) {
+            return { data: null, error: 'Todos los campos son obligatorios! Intenta de nuevo.' }
+        }
+
         const { data, error} = await secureFetch({
             options: {
                 url: `${WheekConfig.API_BASE_URL}/stores/update`,
@@ -32,10 +40,14 @@ export const StoreService = {
             return { data: null, error: error }
         }
 
-        return { data, error: null }
+        return { data: data.value, error: null }
     },
 
     async getStores(user_id: string): Promise<{ data: StoreData[] | null, error: string | null }> {
+        if (!user_id) {
+            return { data: null, error: 'El ID del usuario es obligatorio! Intenta de nuevo.' }
+        }
+
         const { data, error} = await secureFetch({
             options: {
                 url: `${WheekConfig.API_BASE_URL}/stores/get/all/${user_id}`,
