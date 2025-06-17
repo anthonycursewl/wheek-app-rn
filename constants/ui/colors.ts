4e2291a0ed8734174842616ec904e8bcd8bb38a2
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-17 15:58:00 -0400

    Feat: Implement WheekSpecialColors, mix main UI colors

--- constants/ui/colors.ts
+++ constants/ui/colors.ts
@@ -1,7 +1,15 @@
 export const WheekColors = {
-    primary: '#FF0000',
+    primary: '#',
     warning: '#00FF00',
     error: '#0000FF',
     binary: '#000000',
     white: '#FFFFFF',
+}
+
+
+// TODO: try to mix these colors
+export const WheekSpecialColors = {
+    primary:  'rgb(94, 36, 255)',
+    binary: 'rgb(250, 182, 79)',
+    secondary: 'rgb(255, 143, 99)',
 }
