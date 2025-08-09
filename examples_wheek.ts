e698b481cfa17499ef12a0528303ee02cb091af9
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-08-09 01:11:30 -0400

    feat: Add example product and required dependencies

---
+++ examples_wheek.ts
@@ -0,0 +1,17 @@
+/*
+ * Example create product
+ */
+
+const productExample = {
+    "barcode": "7606404532371",
+    "category_id": "65070c81-a42e-4d2f-9876-56410910d77e",
+    "w_ficha": {
+        "benchmark": "5",
+        "condition": "UND",
+        "cost": "8",
+        "tax": true
+    },
+    "name": "Brd+",
+    "provider_id": "67982f99-98ba-4f23-a6c9-51693b250ebb",
+    "store_id": "b8ec7edf-3713-47d1-9aed-879e9593b1e7"
+}
