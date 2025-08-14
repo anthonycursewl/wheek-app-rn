9545cd77b3efc2661eb920606735859f7b8de041
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-08-14 18:12:59 -0400

    feat: Update ProductItem component and ProductManagement list for new product features

--- components/dashboard/products/components/ProductItem.tsx
+++ components/dashboard/products/components/ProductItem.tsx
@@ -1,12 +1,33 @@
-import { TouchableOpacity } from "react-native"
-import { View } from "react-native"
-import CustomText from "@components/CustomText/CustomText"
-import { Product } from "@flux/entities/Product"
+import { TouchableOpacity, View } from "react-native";
+import CustomText from "@components/CustomText/CustomText";
+import { Product } from "@flux/entities/Product";
 
-export const ProductItem = ({ product, onPress }: { product: Product, onPress?: () => void }) => {
+export const ProductItem = ({ product, onPress }: { product: Product, onPress?: (product: Product) => void }) => {
+
+    const finalPriceProduct = (parseFloat(product.w_ficha.cost) + parseFloat(product.w_ficha.cost) * (parseFloat(product.w_ficha.benchmark) / 100)).toFixed(2);
+
+    const isNew = () => {
+        if (!product.created_at) return false;
+        const productDate = new Date(product.created_at);
+        const now = new Date();
+        const differenceInTime = now.getTime() - productDate.getTime();
+        const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+        return differenceInDays <= 3;
+    };
+
+    const nameProduct = () => {
+        const color = isNew() ? 'rgb(71, 71, 71)' : '#1a202c';
+        const text = product.name.length > 20 ? product.name.substring(0, 20) + '...' : product.name;
+        return (
+            <CustomText style={{ fontSize: 18, fontWeight: '600', color, marginRight: 8 }}>
+                {text}
+            </CustomText>
+        )
+    };
+
     return (
-        <TouchableOpacity 
-            onPress={onPress}
+        <TouchableOpacity
+            onPress={() => onPress?.(product)}
             style={{
                 backgroundColor: '#ffffff',
                 borderRadius: 12,
@@ -22,17 +43,44 @@ export const ProductItem = ({ product, onPress }: { product: Product, onPress?: 
             }}
         >
             <View style={{ padding: 16 }}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 8 }}>
-                    <CustomText style={{ fontSize: 18, fontWeight: '600', color: '#1a202c' }}>
-                        {product.name}
-                    </CustomText>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 8 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', position: 'relative' }}>
+                        {isNew() && (
+                            <>
+                                <View style={{ 
+                                    position: 'absolute', 
+                                    top: 0, left: 0, 
+                                    right: 0, bottom: 0,
+                                    height: 50,
+                                    width: 50,
+                                    borderRadius: 100,
+                                    backgroundColor: 'rgba(247, 133, 57, 0.81)',
+                                    filter: 'blur(25px)',
+                                }} />
+
+                                <View style={{ 
+                                    position: 'absolute', 
+                                    top: 0, left: 65, 
+                                    right: 0, bottom: 0,
+                                    height: 40,
+                                    width: 40,
+                                    borderRadius: 100,
+                                    backgroundColor: 'rgba(130, 57, 247, 0.93)',
+                                    filter: 'blur(20px)',
+                                }} />
+                            </>
+                        )}
+                        
+                        {nameProduct()}
+                    </View>
+
                     <View style={{
                         backgroundColor: product.w_ficha.condition === 'KG' ? '#e6fffa' : '#ebf8ff',
                         paddingHorizontal: 8,
                         paddingVertical: 2,
                         borderRadius: 4
                     }}>
-                        <CustomText style={{ 
+                        <CustomText style={{
                             color: product.w_ficha.condition === 'KG' ? '#2c7a7b' : '#2b6cb0',
                             fontSize: 12,
                             fontWeight: '600'
@@ -64,8 +112,8 @@ export const ProductItem = ({ product, onPress }: { product: Product, onPress?: 
                     </View>
                     <View style={{ alignItems: 'flex-end' }}>
                         <CustomText style={{ fontSize: 12, color: '#718096' }}>Impuesto</CustomText>
-                        <CustomText style={{ 
-                            fontSize: 14, 
+                        <CustomText style={{
+                            fontSize: 14,
                             color: product.w_ficha.tax ? '#38a169' : '#e53e3e',
                             fontWeight: '500'
                         }}>
@@ -74,9 +122,16 @@ export const ProductItem = ({ product, onPress }: { product: Product, onPress?: 
                     </View>
                 </View>
 
-                <View style={{ marginTop: 8 }}>
-                    <CustomText style={{ fontSize: 12, color: '#718096' }}>ID del producto</CustomText>
-                    <CustomText style={{ fontSize: 14, color: '#2d3748' }}>{product.id}</CustomText>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 8, alignItems: 'center' }}>
+                    <View style={{ marginTop: 8 }}>
+                        <CustomText style={{ fontSize: 12, color: '#718096' }}>ID del producto</CustomText>
+                        <CustomText style={{ fontSize: 14, color: '#2d3748' }}>{product.id}</CustomText>
+                    </View>
+
+                    <View>
+                        <CustomText style={{ fontSize: 12, color: '#718096' }}>Precio</CustomText>
+                        <CustomText style={{ fontSize: 14, color: 'rgb(54, 190, 100)' }}>${finalPriceProduct}</CustomText>
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
--- components/dashboard/products/management/ProductManagement.tsx
+++ components/dashboard/products/management/ProductManagement.tsx
@@ -1,59 +1,22 @@
 import Button from "@components/Buttons/Button";
-import { FlatList, View, Alert } from "react-native";
-import { useProductStore } from "@flux/stores/useProductStore";
-import { ProductItem } from "../components/ProductItem"; 
+import { View } from "react-native";
 import { router } from "expo-router";
+import ListProducts from "../components/ListProducts";
 import { Product } from "@flux/entities/Product";
-import { useEffect } from "react";
-import { useGlobalStore } from "@flux/stores/useGlobalStore";
-import { ProductService } from "@flux/services/Products/ProductService";
-import { productCreateAttemptAction, productCreateFailureAction } from "@flux/Actions/ProductActions";
 
 export default function ProductManagement() {
-    const { products, dispatch, skip, take, hasMore, loading, error } = useProductStore()
-    const { currentStore } = useGlobalStore()
-
-    const getAllProductsData = async () => {
-        if (products.length !== 0 || !hasMore || loading) return;
-
-        dispatch(productCreateAttemptAction())
-        const { data, error } = await ProductService.getAllProducts(currentStore.id, skip, take)  
-
-        if (error) {
-            dispatch(productCreateFailureAction(error))
-        }
-
-        if (data) {
-            dispatch({ type: 'GET_PRODUCTS_SUCCESS', payload: { products: data, reset: false } })
-        }
-    }
-
-    useEffect(() => {
-        getAllProductsData()
-    }, [])
-
-    useEffect(() => {
-        if (error) Alert.alert(error || 'Error al cargar los productos')
-    }, [error])
-
     const goToProductDetail = (product: Product) => {
         const productString = encodeURIComponent(JSON.stringify(product));
         router.push(`/products/ProductDetail?product=${productString}`);
     }
 
     return (
-        <View style={{ marginTop: 20, gap: 10 }}>
+        <View style={{ marginTop: 15, gap: 10 }}>
             <Button title="Crear producto" 
             onPress={() => router.push('/products/create')}
             />
             
-            <FlatList
-                data={products}
-                renderItem={({ item }) => (
-                    <ProductItem product={item} onPress={() => goToProductDetail(item)} />
-                )}
-                keyExtractor={(item) => item.id}
-            />
+            <ListProducts height={'78%'} onPress={(product) => goToProductDetail(product)} />
         </View>
     )
 }
