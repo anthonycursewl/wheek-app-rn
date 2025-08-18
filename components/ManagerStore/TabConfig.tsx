56e6c2fac1156c0fbbc11ceb803f6e303bb1279c
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-08-17 23:42:20 -0400

    feat: Implement tab configurations for dashboard

---
+++ components/ManagerStore/TabConfig.tsx
@@ -0,0 +1,16 @@
+import IconProducts from 'svgs/IconProducts';
+import IconExpenses from 'svgs/IconExpenses';
+import IconReceipts from 'svgs/IconReceipts';
+import ProductManagement from '@components/dashboard/products/management/ProductManagement';
+import CategoryManagement from '@components/dashboard/categories/management/CategoryManagement';
+import ProviderManagement from '@components/dashboard/providers/management/ProviderManagement';
+import ReceptionManagement from '@components/dashboard/receptions/management/ReceptionManagement';
+import AdjustManagement from '@components/dashboard/adjust/management/AdjustManagement';
+
+export const TABS_CONFIG = [
+  { id: 'products', label: 'Productos', Icon: IconProducts, Component: ProductManagement },
+  { id: 'categories', label: 'Categorias', Icon: IconProducts, Component: CategoryManagement },
+  { id: 'providers', label: 'Proveedores', Icon: IconProducts, Component: ProviderManagement },
+  { id: 'expenses', label: 'Recepciones', Icon: IconExpenses, Component: ReceptionManagement },
+  { id: 'receipts', label: 'Ajustes', Icon: IconReceipts, Component: AdjustManagement },
+];
