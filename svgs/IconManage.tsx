9c79154358a34117d9e747aba33845a55495da9b
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-09-20 11:40:38 -0400

    feat: Refactor IconManage, IconOrder, IconProducts, and IconReceptions components - Update types and import statements to use `SvgProps` for better type safety.

--- svgs/IconManage.tsx
+++ svgs/IconManage.tsx
@@ -1,14 +1,13 @@
-import Svg, { Path, G } from "react-native-svg"
+import Svg, { Path, G, SvgProps } from "react-native-svg"
 
-export default function IconManage(props: any) {
+export default function IconManage({ ...props }: SvgProps) {
   return (
   <Svg
-    xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     {...props}
   >
-    <G fill="#3c3c3c">
+    <G fill={props.fill}>
       <Path d="M8.422 20.618C10.178 21.54 11.056 22 12 22V12L2.638 7.073a3.196 3.196 0 0 0-.04.067C2 8.154 2 9.417 2 11.942v.117c0 2.524 0 3.787.597 4.801.598 1.015 1.674 1.58 3.825 2.709l2 1.05Z" />
       <Path
         d="m17.577 4.432-2-1.05C13.822 2.461 12.944 2 12 2c-.945 0-1.822.46-3.578 1.382l-2 1.05C4.318 5.536 3.242 6.1 2.638 7.072L12 12l9.362-4.927c-.606-.973-1.68-1.537-3.785-2.641Z"
