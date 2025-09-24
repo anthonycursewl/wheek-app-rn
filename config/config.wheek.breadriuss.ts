a1713376e6fa8d85a8680c5a6fa72aca92f6c73f
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-09-24 10:38:49 -0400

    feat: Configuration update - Added Wheek's config file for API base URL

---
+++ config/config.wheek.breadriuss.ts
@@ -0,0 +1,15 @@
+/*
+    Wheek's Config
+    Don't change this file unless you know what you're doing. 
+    If you need to change the API_BASE_URL, change it in the config.wheek.breadriuss.ts file.
+    @author Anthony Cursewl
+    @version 1.0.0
+    @license MIT
+    @copyright 2025 Breadriuss
+
+    https://wheek.breadriuss.com
+*/
+
+export const WheekConfig = {
+    API_BASE_URL: 'http://192.168.0.109:4001/api'
+}
